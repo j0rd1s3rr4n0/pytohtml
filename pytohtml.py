48032762373c4d3d6c8bf02af154d1cb03b83342
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2020-03-13 13:53:25 +0100

    Create pytohtml.py

---
+++ pytohtml.py
@@ -0,0 +1,532 @@
+#######################################################################################################
+## IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT ##
+#######################################################################################################
+import os
+import time
+#######################################################################################################
+## IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT IMPORT ##
+#######################################################################################################
+
+
+
+
+
+
+
+def escritura():
+	filename = input("Escriba nombre del arhivo: ")
+	extension = input("Extension: (example: txt ")
+	add_content = "asd"
+	fichero = open(filename+"."+extension,"w")
+	fichero.write(add_content)
+	fichero.close()
+
+
+
+add_content=" "
+#######################################################################################################
+# HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML #
+#######################################################################################################
+def save_as():
+	filename = input("Escriba nombre del arhivo: ")
+	extension = input("Extension: (example: txt ")
+	fichero = open(filename+"."+extension,"w")
+	fichero.write(add_content)
+	fichero.close()
+
+def save_as_fiveteen():
+	filename = input("Escriba nombre del arhivo: ")
+	extension = input("Extension: (example: txt ")
+	print("DOC "+filename+" - "+" 50 Lines Maximum")
+	line_1 = input(str(" 1 "))
+	line_2 = input(str(" 2 "))
+	line_3 = input(str(" 3 "))
+	line_4 = input(str(" 4 "))
+	line_5 = input(str(" 5 "))
+	line_6 = input(str(" 6 "))
+	line_7 = input(str(" 7 "))
+	line_8 = input(str(" 8 "))
+	line_9 = input(str(" 9 "))
+	line10 = input(str("10 "))
+	line11 = input(str("11 "))
+	line12 = input(str("12 "))
+	line13 = input(str("13 "))
+	line14 = input(str("14 "))
+	line15 = input(str("15 "))
+	line16 = input(str("16 "))
+	line17 = input(str("17 "))
+	line18 = input(str("18 "))
+	line19 = input(str("19 "))
+	line20 = input(str("20 "))
+	line21 = input(str("21 "))
+	line22 = input(str("22 "))
+	line23 = input(str("23 "))
+	line24 = input(str("24 "))
+	line25 = input(str("25 "))
+	line26 = input(str("26 "))
+	line27 = input(str("27 "))
+	line28 = input(str("28 "))
+	line29 = input(str("29 "))
+	line30 = input(str("30 "))
+	line31 = input(str("31 "))
+	line32 = input(str("32 "))
+	line33 = input(str("33 "))
+	line34 = input(str("34 "))
+	line35 = input(str("35 "))
+	line36 = input(str("36 "))
+	line37 = input(str("37 "))
+	line38 = input(str("38 "))
+	line39 = input(str("39 "))
+	line40 = input(str("40 "))
+	line41 = input(str("41 "))
+	line42 = input(str("42 "))
+	line43 = input(str("43 "))
+	line44 = input(str("44 "))
+	line45 = input(str("45 "))
+	line46 = input(str("46 "))
+	line47 = input(str("47 "))
+	line48 = input(str("48 "))
+	line49 = input(str("49 "))
+	line50 = input(str("50 "))
+	os.system('cls')
+	add_content=str("""
+	"""+" "+line_1
+		+"""
+	"""+" "+line_2
+		+"""
+	"""+" "+line_3
+		+"""
+	"""+" "+line_4
+		+"""
+	"""+" "+line_5
+		+"""
+	"""+" "+line_6
+		+"""
+	"""+" "+line_7
+		+"""
+	"""+" "+line_8
+		+"""
+	"""+" "+line_9
+		+"""
+	"""+" "+line10
+		+"""
+	"""+" "+line11
+		+"""
+	"""+" "+line12
+		+"""
+	"""+" "+line13
+		+"""
+	"""+" "+line14
+		+"""
+	"""+" "+line15
+		+"""
+	"""+" "+line16
+		+"""
+	"""+" "+line17
+		+"""
+	"""+" "+line18
+		+"""
+	"""+" "+line19
+		+"""
+	"""+" "+line20
+		+"""
+	"""+" "+line21
+		+"""
+	"""+" "+line22
+		+"""
+	"""+" "+line23
+		+"""
+	"""+" "+line24
+		+"""
+	"""+" "+line25
+		+"""
+	"""+" "+line26
+		+"""
+	"""+" "+line27
+		+"""
+	"""+" "+line28
+		+"""
+	"""+" "+line29
+		+"""
+	"""+" "+line30
+		+"""
+	"""+" "+line31
+		+"""
+	"""+" "+line32
+		+"""
+	"""+" "+line33
+		+"""
+	"""+" "+line34
+		+"""
+	"""+" "+line35
+		+"""
+	"""+" "+line36
+		+"""
+	"""+" "+line37
+		+"""
+	"""+" "+line38
+		+"""
+	"""+" "+line39
+		+"""
+	"""+" "+line40
+		+"""
+	"""+" "+line41
+		+"""
+	"""+" "+line42
+		+"""
+	"""+" "+line43
+		+"""
+	"""+" "+line44
+		+"""
+	"""+" "+line45
+		+"""
+	"""+" "+line46
+		+"""
+	"""+" "+line47
+		+"""
+	"""+" "+line48
+		+"""
+	"""+" "+line49
+		+"""
+	"""+" "+line50)
+	print(add_content)
+	fichero = open(filename+"."+extension,"w")
+	fichero.write(add_content)
+	fichero.close()
+def CssName():
+	print("Insert CSS File (C0DE)")
+	name_css_c = input("CSS NAME: ")
+
+def HTML_TITLE():
+	print("Insert HTML TITLE")
+	title_name_c = input("TITLE: ")
+
+def COD_TEXT():
+	cod_text_c = input (''' Which coding do you want to use?
+			ASCII    ANSI    ISO-8859-1    UTF-8
+ Codification Name: ''')
+
+def HTML():
+	print(" ")
+	print("<html>")
+	print("	<head>")
+	print("<title>"+title_name_c+"</title>")
+	print("""
+	In .html
+		<meta charset="""+'"'+cod_text_c+'"'+""">
+	
+		OR
+	
+	In .css:
+		@charset """+cod_text_c+""";
+	""")
+	print("""<link rel="stylesheet" type="text/css" href="""+'"'+name_css_c+".css"+'"'+">""")
+	print("""	<head>
+		<body>
+		<!-- Enter your content code here -->
+		<!-- Example content code -->
+		<h1>Example title</h1>
+		<font class="class1">EXAMPLE</font> <font class="class2">TEXT</font>
+		<!-- End Example content code -->
+		</body>
+		</html>
+		""")
+#######################################################################################################
+# HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML HTML #
+#######################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+#######################################################################################################
+# CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS #
+#######################################################################################################
+def css_style():
+		Css_style = input(str("Class Name: "))
+	
+		print("""
+ (0)  font-weight        (3)  font-variant       (6)  color              (9)  font-family
+ (1)  text-decoration    (4)  letter-spacing     (7)  font-size          
+ (2)  text-transofrom    (5)  background-color   (8)  font-style            
+
+			""")
+
+		propiety_instyle = input(str(" Propiety Name: "))
+		#Propiedades
+
+		if propiety_instyle == "font-weight":
+			print("""
+ { bolder / bold / normal / light / lighter | inherit / initial / unset / 500 / 600 / 700 / 800 / 900}
+			""")
+
+		elif propiety_instyle == "text-decoration":
+			print("""
+ { line-through overline underline | Dashed / Dotted / Double / None / Solid / Wavy }
+			""")
+
+		elif propiety_instyle == "font-variant" :
+			print("""
+ { normal / small-caps | inherit / initial / unset }
+			""")
+
+		elif propiety_instyle == "letter-spacing" :
+			print("""
+ { normal / 5 px / 5 cm / 5 mm / 5 rem }
+			""")
+
+		elif propiety_instyle == "background-color":
+			print("""
+ { Name Color or Color Code with # before }
+			""")
+
+		elif propiety_instyle == "color":
+			print("""
+ { Name Color or Color Code with # before }
+			""")
+
+		elif propiety_instyle == "font-size":
+			print("""
+ { xx-large / x-large / larger / large / medium / small / smaller / x-small / xx-small }
+			""")
+
+
+		elif propiety_instyle == "font-style" :
+			print("""
+ { italic / normal / oblique | inherit / initial / unset }
+			""")
+
+		elif propiety_instyle == "font-family":
+			print("""
+ Arial, sans-serif	
+ Helvetica, sans-serif	
+ Verdana, sans-serif	
+ Trebuchet MS, sans-serif	
+ Gill Sans, sans-serif	
+ Noto Sans, sans-serif	
+ Avantgarde, TeX Gyre Adventor, URW Gothic L, sans-serif	
+ Optima, sans-serif	
+ Arial Narrow, sans-serif	
+ sans-serif
+ Times, Times New Roman, serif	
+ Didot, serif	
+ Georgia, serif	
+ Palatino, URW Palladio L, serif	
+ Bookman, URW Bookman L, serif	
+ New Century Schoolbook, TeX Gyre Schola, serif	
+ American Typewriter, serif	
+ serif	
+ Andale Mono, monospace	
+ Courier New, monospace	
+ Courier, monospace	
+ FreeMono, monospace	
+ OCR A Std, monospace	
+ DejaVu Sans Mono, monospace	
+ monospace	
+ Comic Sans MS, Comic Sans, cursive	
+ Apple Chancery, cursive	
+ Bradley Hand, cursive	
+ Brush Script MT, Brush Script Std, cursive	
+ Snell Roundhand, cursive	
+ URW Chancery L, cursive	
+ cursive	
+ Impact, fantasy	
+ Luminari, fantasy	
+ Chalkduster, fantasy	
+ Jazz LET, fantasy	
+ Blippo, fantasy	
+ Stencil Std, fantasy	
+ Marker Felt, fantasy	
+ Trattatello, fantasy											
+ fantasy""")
+
+		propiety_instyle_1 = input(str(" Propiety Code: "))
+		os.system('cls')
+		print(propiety_instyle+":"+propiety_instyle_1+";")
+		print("""
+ (0)  font-weight        (3)  font-variant       (6)  color              (9)  font-family
+ (1)  text-decoration    (4)  letter-spacing     (7)  font-size          
+ (2)  text-transofrom    (5)  background-color   (8)  font-style            
+
+			""")
+		propiety_instyle_2 = input(str(" Propiety Name: "))
+
+		if propiety_instyle_2 == "font-weight":
+				print("""
+ { bolder / bold / normal / light / lighter | inherit / initial / unset / 500 / 600 / 700 / 800 / 900}
+			""")
+
+		elif propiety_instyle_2 == "text-decoration":
+				print("""
+ { line-through overline underline | Dashed / Dotted / Double / None / Solid / Wavy }
+			""")
+
+		elif propiety_instyle_2 == "font-variant" :
+			print("""
+ { normal / small-caps | inherit / initial / unset }
+			""")
+
+		elif propiety_instyle_2 == "letter-spacing" :
+			print("""
+ { normal / 5 px / 5 cm / 5 mm / 5 rem }
+			""")
+
+		elif propiety_instyle_2 == "background-color":
+				print("""
+ { Name Color or Color Code with # before }
+			""")
+
+		elif propiety_instyle_2 == "color":
+				print("""
+ { Name Color or Color Code with # before }
+			""")
+
+		elif propiety_instyle_2 == "font-size":
+			print("""
+ { xx-large / x-large / larger / large / medium / small / smaller / x-small / xx-small }
+			""")
+
+
+		elif propiety_instyle_2 == "font-style" :
+			print("""
+ { italic / normal / oblique | inherit / initial / unset }
+			""")
+
+		elif propiety_instyle_2 == "font-family":
+			print("""
+ Arial, sans-serif	
+ Helvetica, sans-serif	
+ Verdana, sans-serif	
+ Trebuchet MS, sans-serif	
+ Gill Sans, sans-serif	
+ Noto Sans, sans-serif	
+ Avantgarde, TeX Gyre Adventor, URW Gothic L, sans-serif	
+ Optima, sans-serif	
+ Arial Narrow, sans-serif	
+ sans-serif
+ Times, Times New Roman, serif	
+ Didot, serif	
+ Georgia, serif	
+ Palatino, URW Palladio L, serif	
+ Bookman, URW Bookman L, serif	
+ New Century Schoolbook, TeX Gyre Schola, serif	
+ American Typewriter, serif	
+ serif	
+ Andale Mono, monospace	
+ Courier New, monospace	
+ Courier, monospace	
+ FreeMono, monospace	
+ OCR A Std, monospace	
+ DejaVu Sans Mono, monospace	
+ monospace	
+ Comic Sans MS, Comic Sans, cursive	
+ Apple Chancery, cursive	
+ Bradley Hand, cursive	
+ Brush Script MT, Brush Script Std, cursive	
+ Snell Roundhand, cursive	
+ URW Chancery L, cursive	
+ cursive	
+ Impact, fantasy	
+ Luminari, fantasy	
+ Chalkduster, fantasy	
+ Jazz LET, fantasy	
+ Blippo, fantasy	
+ Stencil Std, fantasy	
+ Marker Felt, fantasy	
+ Trattatello, fantasy											
+ fantasy""")
+		propiety_instyle_2_2 = input(str(" Propiety Code: "))
+		os.system('cls')
+		print(" ")
+		print(" "+propiety_instyle+":"+propiety_instyle_1+";")
+		print(" "+propiety_instyle_2+":"+propiety_instyle_2_2+";")
+		spam = input('Press Enter To Continue')
+		os.system('cls')
+		print(" Contenido del documento style.css")
+		print(" ")
+		un = "  ."+Css_style+"{"
+		dos ="   "+propiety_instyle+":"+propiety_instyle_1+";"
+		tres ="   "+propiety_instyle_2+":"+propiety_instyle_2_2+";"
+		quatre ="  }"
+		print(un)
+		print(dos)
+		print(tres)
+		print(quatre)
+		spam = input('Press Enter To Continue')
+		os.system('cls')
+
+
+
+#######################################################################################################
+# CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS   CSS #
+#######################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+#######################################################################################################
+# EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE #
+#######################################################################################################
+
+def Menu():
+	os.system('cls')
+	print('¿QUIERES APROBAR HTML? -- AQUI ESTÀ TU CHULETILLA')
+	print("""
+	(0) GUIDED MODE
+	(1) OUR OWN         
+	(2) HTML                
+	(3) CSS                 
+	(9) CREADOR             
+	""")
+	menu = input(str("OPTION NUMBER: "))
+
+	if menu == "0":
+		print("""
+		#css_guided()
+		#title_guided()
+		#codification_guided()
+		#title_guided()
+		#link_css_guided()	
+		#content_guided()
+		#save_as()
+		""")
+	elif menu =="1":
+		doc_name = input(str("Doc Name: "))
+		
+		save_as_fiveteen()
+
+	elif menu =="2":
+		CssName()
+		HTML_TITLE()
+		COD_TEXT()
+		HTML()
+		save_as()
+	elif menu =="3":
+		css_style()
+	
+	elif menu =="4":
+		escritura()
+		
+	elif menu =="9":
+		print('Created By Jordi Serrano')
+
+	else:
+		print("PARAMETRO NO FUNCIONAL")
+
+Menu()
+#######################################################################################################
+# EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE EXE #
+#######################################################################################################
